104384cc3b51665fa757eb7955bc0763e29d05af
StephenStolk <shikharburman2@gmail.com>
2025-08-22 22:38:39 +0530

    Made register route

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -16,7 +16,30 @@ export async function POST(req:NextRequest) {
             })
         };
 
-        
+        const existingUser = await User.findOne({
+            email
+        });
+
+        if(existingUser){
+            return NextResponse.json({
+                error: "user aalready there"
+            }, {status: 400})
+        };
+
+        const newUser = new User({
+            username, email, password
+        });
+
+        const saveduser = await newUser.save();
+
+        if(saveduser) {
+            return NextResponse.json({
+                message: "User registered successfully",
+                success: true, saveduser
+            }, {
+                status: 201
+            });
+        }
 
     } catch (error) {
         return NextResponse.json({
